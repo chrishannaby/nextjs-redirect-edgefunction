74f857bed687c6595284256b8060b73576d64167
Chris Hannaby <chris.hannaby@netlify.com>
2022-08-23 15:54:29 -0600

    use full url

--- netlify/edge-functions/proxy.ts
+++ netlify/edge-functions/proxy.ts
@@ -1,6 +1,7 @@
 import { Context } from "netlify:edge";
 const pathRegex = /^https:\/\/www2-stage.aapc.com/;
-const proxyUrl = "/.netlify/functions/login";
+const proxyUrl =
+  "https://redirect-via-edge-function.netlify.app/.netlify/functions/login";
 
 export default async (request: Request, context: Context) => {
   const response = await fetch(proxyUrl);
