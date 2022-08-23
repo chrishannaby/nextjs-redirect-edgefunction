5446c41183786fe96310877468ba0f87a5ecf923
Chris Hannaby <chris.hannaby@netlify.com>
2022-08-23 16:20:35 -0600

    clone headers

--- netlify/edge-functions/proxy.ts
+++ netlify/edge-functions/proxy.ts
@@ -1,7 +1,19 @@
 import { Context } from "netlify:edge";
-const proxyUrl = "https://www2-stage.aapc.com/account";
+const pathRegex = /^https:\/\/www2-stage.aapc.com/;
+const proxyUrl =
+  "https://redirect-via-edge-function.netlify.app/.netlify/functions/login";
 
 export default async (request: Request, context: Context) => {
-  const response = await fetch(proxyUrl);
+  const response = await fetch(proxyUrl, { redirect: "manual" });
+  const locationHeader = response.headers.get("Location");
+  if (locationHeader && pathRegex.test(locationHeader)) {
+    const newHeaders = new Headers(response.headers);
+    newHeaders.set("Location", locationHeader.replace(pathRegex, ""));
+    return new Response(response.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: newHeaders,
+    });
+  }
   return response;
 };
