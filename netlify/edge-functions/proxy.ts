7ffc1f29d89b1e67fa8a6b06f5dcb4137a2921ed
Chris Hannaby <chris.hannaby@netlify.com>
2022-08-23 16:07:45 -0600

    try something else

--- netlify/edge-functions/proxy.ts
+++ netlify/edge-functions/proxy.ts
@@ -1,15 +1,7 @@
 import { Context } from "netlify:edge";
-const pathRegex = /^https:\/\/www2-stage.aapc.com/;
-const proxyUrl =
-  "https://redirect-via-edge-function.netlify.app/.netlify/functions/login";
+const proxyUrl = "https://www2-stage.aapc.com/account";
 
 export default async (request: Request, context: Context) => {
-  const response = await fetch(proxyUrl, { redirect: "manual" });
-  console.log(response);
-  const locationHeader = response.headers.get("Location");
-  if (locationHeader && pathRegex.test(locationHeader)) {
-    response.headers.set("Location", locationHeader.replace(pathRegex, ""));
-  }
-  console.log(response.headers);
+  const response = await fetch(proxyUrl);
   return response;
 };
