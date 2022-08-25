c1075caff4c646a51b052a7f1fc9253f312d63b5
Chris Hannaby <chris.hannaby@netlify.com>
2022-08-25 15:36:20 -0600

    clone request

--- netlify/edge-functions/proxy.ts
+++ netlify/edge-functions/proxy.ts
@@ -7,8 +7,9 @@ export default async (request: Request, context: Context) => {
   const requestPath = new URL(request.url).pathname;
   const requestQuery = new URL(request.url).search;
   const proxyUrl = proxyUrlBase + requestPath + requestQuery;
-  console.log(proxyUrl);
-  const response = await fetch(proxyUrl, { redirect: "manual" });
+  const newRequest = new Request(proxyUrl, request);
+  console.log(newRequest);
+  const response = await fetch(request, { redirect: "manual" });
   const locationHeader = response.headers.get("Location");
   if (locationHeader && pathRegex.test(locationHeader)) {
     const newHeaders = new Headers(response.headers);
