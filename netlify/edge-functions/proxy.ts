c49dde4c168515b7b0ed4c09f87bc7941dcbc931
Chris Hannaby <chris.hannaby@netlify.com>
2022-08-25 15:21:33 -0600

    remove function route

--- netlify/edge-functions/proxy.ts
+++ netlify/edge-functions/proxy.ts
@@ -1,7 +1,7 @@
 import { Context } from "netlify:edge";
 const pathRegex = /^https:\/\/www2-stage.aapc.com/;
 const proxyUrlBase =
-  "https://redirect-via-edge-function.netlify.app/.netlify/functions/login";
+  "https://redirect-via-edge-function.netlify.app/.netlify/functions";
 
 export default async (request: Request, context: Context) => {
   const requestPath = new URL(request.url).pathname;
