3a68005a53ba9c25b4b61212623f32cee9e428b1
Chris Hannaby <chris.hannaby@netlify.com>
2022-08-30 15:27:32 -0600

    minimal cookie example

--- netlify/edge-functions/proxy.ts
+++ netlify/edge-functions/proxy.ts
@@ -1,22 +1,9 @@
 import { Context } from "netlify:edge";
-const pathRegex = /^https:\/\/www2-stage.aapc.com/;
-const proxyUrlBase = "https://www.moneytronicswag.com/.netlify/functions";
 
 export default async (request: Request, context: Context) => {
-  const requestPath = new URL(request.url).pathname;
-  const requestQuery = new URL(request.url).search;
-  const proxyUrl = proxyUrlBase + requestPath + requestQuery;
-  const response = await fetch(proxyUrl, { redirect: "manual" });
-  const locationHeader = response.headers.get("Location");
-  if (locationHeader && pathRegex.test(locationHeader)) {
-    const newHeaders = new Headers(response.headers);
-    newHeaders.set("Location", locationHeader.replace(pathRegex, ""));
-    console.log(newHeaders);
-    return new Response(response.body, {
-      status: response.status,
-      statusText: response.statusText,
-      headers: newHeaders,
-    });
-  }
-  return response;
+  context.cookies.set({
+    name: "netlify",
+    value: "hello",
+    domain: "moneytronicswag.com",
+  });
 };
