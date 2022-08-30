2203ecec11900e38c50b070ebe3859605d3cb8d4
Chris Hannaby <chris.hannaby@netlify.com>
2022-08-30 13:05:45 -0600

    revert some stuff

--- netlify/edge-functions/proxy.ts
+++ netlify/edge-functions/proxy.ts
@@ -6,13 +6,7 @@ export default async (request: Request, context: Context) => {
   const requestPath = new URL(request.url).pathname;
   const requestQuery = new URL(request.url).search;
   const proxyUrl = proxyUrlBase + requestPath + requestQuery;
-  const newRequest = new Request(proxyUrl, {
-    ...request,
-    redirect: "manual",
-  });
-  console.log(newRequest);
-  const response = await fetch(request);
-  console.log(response);
+  const response = await fetch(proxyUrl, { redirect: "manual" });
   const locationHeader = response.headers.get("Location");
   if (locationHeader && pathRegex.test(locationHeader)) {
     const newHeaders = new Headers(response.headers);
